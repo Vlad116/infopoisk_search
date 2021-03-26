37b1d072e990ed6cc644aad86623b216fc50d8c5
Vladislav <vladalekseev9@yandex.ru>
2021-03-27 01:18:28 +0300

    :sparkles: :recycle: add new feature in boolean_searsh and init tf-idf

--- task3_boolean_search/boolean_search_by_inverting_index.py
+++ task3_boolean_search/boolean_search_by_inverting_index.py
@@ -1,15 +1,18 @@
 from os import listdir
 from os.path import isfile, join
 import re
+import time
+import pymorphy2
 from nltk.corpus import stopwords
-from nltk import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from string import punctuation
 
 # nltk.download("stopwords")  # used only for first time
 
 # 1. Создать инвертированный список терминов (индекс)
+start_time = time.time()
 russian_stopwords = stopwords.words("russian")
-wnl = WordNetLemmatizer()
-
+morph = pymorphy2.MorphAnalyzer()
 
 def process_files(filenames):
     file_to_terms = {}
@@ -17,14 +20,12 @@ def process_files(filenames):
         pattern = re.compile('[\W_]+')
         file_to_terms[file] = open(files_path + '/' + file, 'r', encoding='utf-8').read().lower()
         file_to_terms[file] = pattern.sub(' ', file_to_terms[file])
-        re.sub(r'[\W_]+', '', file_to_terms[file])
-        re.sub(r"[\n\s.,:–\\?—\-!()/«»'#№{}\[\]→%|+®©\"]+", " ", file_to_terms[file], flags=re.UNICODE)
-        re.sub(r"[\d+]", "", file_to_terms[file], flags=re.UNICODE)
-        file_to_terms[file] = file_to_terms[file].split()
-        file_to_terms[file] = [w for w in file_to_terms[file] if w not in russian_stopwords]
-        file_to_terms[file] = [wnl.lemmatize(w) for w in file_to_terms[file]]
+        file_to_terms[file] = word_tokenize(file_to_terms[file], language="russian")
+        file_to_terms[file] = [w for w in file_to_terms[file] if w not in russian_stopwords
+                               and w != " " \
+                               and w.strip() not in punctuation]
+        file_to_terms[file] = [morph.parse(w)[0].normal_form for w in file_to_terms[file]]
     return file_to_terms
-
 
 # input = [word1, word2, ...]
 # output = {word1: [pos1, pos2], word2: [pos2, pos434], ...}
@@ -39,7 +40,6 @@ def index_one_file(termlist):
 
     return fileIndex
 
-
 # input = {filename: [word1, word2, ...], ...}
 # res = {filename: {word: [pos1, pos2, ...]}, ...}
 def make_indices(termlists):
@@ -47,7 +47,6 @@ def make_indices(termlists):
     for filename in termlists.keys():
         total[filename] = index_one_file(termlists[filename])
     return total
-
 
 # input = {filename: {word: [pos1, pos2, ...], ... }}
 # res = {word: {filename: [pos1, pos2]}, ...}, ...}
@@ -64,7 +63,6 @@ def fullIndex(regdex):
                 total_index[word] = {filename: regdex[filename][word]}
     return total_index
 
-
 files_path = '../files'
 files = [f for f in listdir(files_path) if isfile(join(files_path, f))]
 files_len = len(files)
@@ -76,37 +74,51 @@ for i in range(files_len):
 file_to_terms = process_files(file_names)
 regular_index = make_indices(file_to_terms)
 total_index = fullIndex(regular_index)
+print("Inverted Index has been prepared and it took:")
+print('{} seconds'.format(time.time() - start_time))\
 
 # 2. Реализовать булев поиск по построенному индексу
+
+# For an 'PH' (phrase) query - по сути как OR только еще и учитывается последовательность слов (
+# For an 'OR' query, all the documents for every query term is appended to the list of results.
+# For an 'AND' query, we walk through the sorted lists and only the documents present in all the lists are appended to the result-list.
+
 def one_word_query(word):
     pattern = re.compile('[\W_]+')
     word = pattern.sub(' ', word)
+
     if word in total_index.keys():
         return [filename for filename in total_index[word].keys()]
     else:
         return []
 
+# 'OR'
 def free_text_query(string):
-    pattern = re.compile('[\W_]+')
-    string = pattern.sub(' ', string)
     result = []
-    for word in string.split():
+    for word in prepare_query(string):
         result += one_word_query(word)
     return list(set(result))
 
+# 'AND'
+def strict_text_query(string):
+    result = []
+    for word in prepare_query(string):
+        result = list(set(result) & set(one_word_query(word))) if result\
+            else set(one_word_query(word))
+    return result
 
 def phrase_query(string):
-    pattern = re.compile('[\W_]+')
-    string = pattern.sub(' ', string)
+
+    prepared_query = prepare_query(string)
     listOfLists, result = [], []
 
-    for word in string.split():
+    for word in prepared_query:
         listOfLists.append(one_word_query(word))
     setted = set(listOfLists[0]).intersection(*listOfLists)
 
     for filename in setted:
         temp = []
-        for word in string.split():
+        for word in prepared_query:
             temp.append(total_index[word][filename][:])
         for i in range(len(temp)):
             for ind in range(len(temp[i])):
@@ -115,6 +127,40 @@ def phrase_query(string):
             result.append(filename)
     return [result,string]
 
-print(one_word_query('новости'))
+def prepare_query(string):
+    string_words = word_tokenize(string, language="russian")
+    string_words = [w for w in string_words if w not in russian_stopwords
+                    and w != " " \
+                    and w.strip() not in punctuation]
+    string_tokens = [morph.parse(w)[0].normal_form for w in string_words]
+    return string_tokens
+
+print(strict_text_query('новости'))
+print('OR')
 print(free_text_query('новости спорт'))
+print('AND')
+print(strict_text_query('новости спорт футбол'))
+print('Phrase')
 print(phrase_query('главные новости'))
+
+# Getting the query from the user
+query = input("Enter your query string: ")
+query_operator = input("Enter the operator, (AND/OR/PH) Default is AND: ")
+start_time = time.time()
+result_file_indices=[]
+
+if query_operator.lower()=='or':
+    result_file_indices = free_text_query(query)
+elif query_operator.lower()=='ph':
+    result_file_indices = phrase_query(query)
+else:
+    result_file_indices = strict_text_query(query)
+
+print('Time taken to search')
+print('{} seconds'.format(time.time() - start_time))
+
+if(len(result_file_indices) == 0):
+    print("Sorry No matches found")
+else:
+    print('Number of search results: ',len(result_file_indices))
+    print(result_file_indices)
